9110f9dec901b36f9a4f79b5c86e25330c7b433d
fann133 <irfanfann005@gmail.com>
2026-08-06 17:07:57 +0900

    tambahan_shorebird

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -12,9 +12,9 @@ class AppConstants {
   /// - Android emulator memakai 10.0.2.2 agar bisa mengakses localhost host.
   /// - Windows/web/desktop memakai localhost langsung.
   static String get baseUrl {
-    if (kIsWeb) return 'http://localhost:8000/api';
-    if (Platform.isAndroid) return 'http://10.0.2.2:8000/api';
-    return 'http://localhost:8000/api';
+    if (kIsWeb) return 'https://inventaris.mhfann.my.id/api';
+    if (Platform.isAndroid) return 'https://inventaris.mhfann.my.id/api';
+    return 'https://inventaris.mhfann.my.id/api';
   }
 
   static const int perPage = 15;
